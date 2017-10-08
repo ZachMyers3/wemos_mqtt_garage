f085f22e32ad341a7f479d8f0c92eb266ec84192
Zach Myers <ZachMyers3@gmail.com>
2017-10-08 17:24:24 -0400

    Added comments and cleaned up wifi.h

--- src/wifi.h
+++ src/wifi.h
@@ -6,17 +6,16 @@
 #include <ESP8266mDNS.h>
 #include "debug.h"
 
-// variables needed for ESPWifi
+// Adjust these variables for static IP
 const IPAddress STATIC_IP(192, 168, 1, 225);
 const IPAddress GATEWAY_IP(192, 168, 1, 1);
 const IPAddress SUBNET_IP(255, 255, 255, 0);
+// Adjust these variables to connect to desired network
 const char* WIFI_SSID = "Pretty Fly For a Wi-Fi";
 const char* WIFI_PASS = "freckles";
 
 void wifi_setup() {
-  delay(5000);
-  // We start by connecting to a WiFi network
-  INFO_LOG("[WIFI] Connecting to %s\n", WIFI_SSID);
+  INFO_LOG("[WIFI] Connecting to %s ", WIFI_SSID);
   // checks if already connected before WiFi.begin() to prevent loop
   WiFi.mode(WIFI_STA);
   WiFi.config(STATIC_IP, GATEWAY_IP, SUBNET_IP);
@@ -31,7 +30,7 @@ void wifi_setup() {
     INFO_LOG(".");
   }
   randomSeed(micros());
-  INFO_LOG("[WIFI] Successfully connected\n");
+  INFO_LOG("\n[WIFI] Successfully connected\n");
 }
 
 #endif
