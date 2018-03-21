f640d9974fc662c593ecd9e38106cbaff91c695d
ZachMyers3 <ZachMyers3@gmail.com>
2018-03-21 16:13:14 -0400

    Update mqtt.h

--- src/mqtt.h
+++ src/mqtt.h
@@ -14,6 +14,8 @@ const char* MQTT_USERNAME = "";
 const char* MQTT_PASSWORD = "";
 
 void mqtt_connect() {
+  espClient = new WifiClient();
+  client.setClient(*espClient);
   INFO_LOG("[MQTT] Connecting to %s\n", MQTT_SERVER);
   if (client.connect("ESP8266Client", MQTT_USERNAME, MQTT_PASSWORD)) {
     INFO_LOG("[MQTT] Connected\n");
@@ -21,4 +23,5 @@ void mqtt_connect() {
     ERROR_LOG("[MQTT] Connect failed rc=%s, trying again\n", client.state());
     delay(2000);
   }
+  delete(espClient);
 }
